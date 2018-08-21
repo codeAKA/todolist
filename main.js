8d1490d213ae616f009890018f50c567684a2de7
codeAKA <andrzej2adamowicz@gmail.com>
2018-08-21 23:48:21 +0200

    add functions

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
+const addBtn = document.querySelector('.add-btn');
+const magnBtn = document.querySelector('.search-btn');
 const arrowBtn = document.querySelector('.arrow-btn');
-const magnifi = document.querySelector('.search-btn');
+const submenuBtn = document.querySelector('.submenu-item');
+const submenu = document.querySelector('.submenu');
 const searchInput = document.querySelector('.search-inp');
+
+
+function createNote() {
+    
+    getCont();
+    dispCreateNote();
+
+}
+
+function dispCreateNote() {
+
+    const appContainer = document.querySelector('.app-box');
+    const noteContainer = document.querySelector('.text-box');
+
+    if (noteContainer.classList.contains('hide-box')) {
+        noteContainer.classList.remove('hide-box');
+        appContainer.classList.add('hide-box');
+        
+    } else {
+        noteContainer.classList.add('hide-box');
+        appContainer.classList.remove('hide-box');
+    }
+    
+}
 
 function displSearching() {
 
@@ -10,18 +37,31 @@ function displSearching() {
         searchInput.style.display = 'none';
     }
 
-    // console.log('i\'m ready')
 }
 
-magnifi.addEventListener('click', displSearching)
+function displSubmenu() {
+
+    if (submenu.style.display === 'none') {
+        submenu.style.display = 'flex';
+    } else {
+        submenu.style.display = 'none';
+    }
+
+}
 
 function getCont() {    
     
     let noteCon = document.querySelector('.text-cont').value;
 
-    let savedNote = localStorage.setItem('todo', JSON.stringify(noteCon));
+    localStorage.setItem('todo', JSON.stringify(noteCon));
+
+    if (noteCon !== "") {
+        noteCon = "";
+    }
     
-    // console.log(itemContent);
 }
 
-arrowBtn.addEventListener('click', getCont)
+magnBtn.addEventListener('click', displSearching);
+addBtn.addEventListener('click', dispCreateNote);
+arrowBtn.addEventListener('click', createNote);
+submenuBtn.addEventListener('click', displSubmenu);
